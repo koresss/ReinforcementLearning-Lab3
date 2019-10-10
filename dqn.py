a418aa25521884c6fea4e823a6b41c4af5aad896
Dragos Grama <grama.dragos.ioan@gmail.com>
2019-10-10 17:27:05 +0200

    Implemented Combined Replay

--- dqn.py
+++ dqn.py
@@ -14,14 +14,17 @@ gamma         = 0.98
 buffer_limit  = 50000
 batch_size    = 32
 
+
 class ReplayBuffer():
     def __init__(self):
         self.buffer = collections.deque(maxlen=buffer_limit)
     
     def put(self, transition):
         self.buffer.append(transition)
-    
-    def sample(self, n):
+        
+        
+    # Normal Experience Replay
+    def NER_sample(self, n):
         mini_batch = random.sample(self.buffer, n)
         s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
         
@@ -32,6 +35,32 @@ class ReplayBuffer():
             r_lst.append([r])
             s_prime_lst.append(s_prime)
             done_mask_lst.append([done_mask])
+
+        return torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), \
+               torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), \
+               torch.tensor(done_mask_lst)
+               
+    # Combined Experience Replay
+    def CER_sample(self, n):
+        mini_batch = random.sample(self.buffer, n-1)
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
+        
+        for transition in mini_batch:
+            s, a, r, s_prime, done_mask = transition
+            s_lst.append(s)
+            a_lst.append([a])
+            r_lst.append([r])
+            s_prime_lst.append(s_prime)
+            done_mask_lst.append([done_mask])
+            
+        # ADD LAST ONLINE TRANSITION            
+        last_transition = self.buffer[-1]
+        s, a, r, s_prime, done_mask = last_transition
+        s_lst.append(s)
+        a_lst.append([a])
+        r_lst.append([r])
+        s_prime_lst.append(s_prime)
+        done_mask_lst.append([done_mask])
 
         return torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), \
                torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), \
@@ -61,7 +90,7 @@ class Qnet(nn.Module):
             
 def train(q, q_target, memory, optimizer):
     for i in range(10):
-        s,a,r,s_prime,done_mask = memory.sample(batch_size)
+        s,a,r,s_prime,done_mask = memory.NER_sample(batch_size)
 
         q_out = q(s)
         q_a = q_out.gather(1,a)
