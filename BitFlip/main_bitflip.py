5a7cb8ed2aa63a7c8278e55bd8ca6c2ddd24a608
Stefan Bogdan <stefanbogdan180394@gmail.com>
2019-10-14 14:04:15 +0200

    Added extra comments to BitFlip

--- BitFlip/main_bitflip.py
+++ BitFlip/main_bitflip.py
@@ -6,6 +6,7 @@ import numpy as np
 if __name__ == "__main__":
 	runs = 5
 	num_episodes = 3000
+	# Hyperparameters of the Experience Replay
 	batch_size = 128
 	buf_size = 50.000
 
@@ -39,7 +40,7 @@ if __name__ == "__main__":
 				,buf_size_=buf_size
 				)
 			)
-
+	# TODO: Add PER
 	# TODO: Compute Standard error of the mean for each interval
 	# TODO: Plot means for all the runs with the confidence interval
 	# TODO: Use 3 different hyperparameters for buf_size and batch_size
